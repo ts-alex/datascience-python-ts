7951b2ce0ab78c1cb398e6b9c84d08edce0a4983
alex <alexandraagius@gmail.com>
2025-09-26 18:57:31 -0400

    fix generate-requirements downgrade numpy

--- generate-requirements-full.sh
+++ generate-requirements-full.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Run this script to update requirements-core.txt.
 # It uses Docker to ensure that the environment matches what will be used in the production image.
+# in wsl run  bash generate-requirements-full.sh
 set -e
 docker compose run --rm update-deps /bin/sh -c "uv pip compile --output-file=requirements-full.txt --upgrade requirements-core.txt"
